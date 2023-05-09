c3251e1d0b9df890cb8412ece4b2a3b53e5d57e3
Andrei Ionita <andrei.ionita2003@gmail.com>
2023-05-10 00:35:07 +0200

    Modified form to include all filters and added boostrap.

--- forms.py
+++ forms.py
@@ -1,10 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired, AnyOf, NumberRange
 
 city_list = ['Delft', 'Amsterdam']
 
 class FilterForm(FlaskForm):
     city = StringField('City', validators=[DataRequired(), AnyOf(city_list)])
-    bedrooms = StringField('No. of rooms', validators=[NumberRange(min=1)])
-
+    rooms = StringField('No. of rooms', validators=[NumberRange(min=1)])
+    min_price = StringField('Minimum price', validators=[NumberRange(min=0)])
+    max_price = StringField('Maximum price', validators=[NumberRange(min=1)])
+    interiorType = SelectField('Interior/Furniture', choices=['Furnished', 'Unfurnished'])
+    size = StringField('Minimum size', validators=[DataRequired()])
+    submit = SubmitField('Search for apartments')
