fc4efef2f3ee7e7e60f166223b2c9bbb5f61c0b7
Andrei Ionita <andrei.ionita2003@gmail.com>
2023-05-12 00:41:50 +0200

    Added functionality for redirecting to results page in the case of a successful input of filters.

--- forms.py
+++ forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField
+from wtforms import StringField, SelectField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, AnyOf, NumberRange
 
 city_list = ['Delft', 'Amsterdam']
 
 class FilterForm(FlaskForm):
-    city = StringField('City', validators=[DataRequired(), AnyOf(city_list)])
-    rooms = StringField('No. of rooms', validators=[NumberRange(min=1)])
-    min_price = StringField('Minimum price', validators=[NumberRange(min=0)])
-    max_price = StringField('Maximum price', validators=[NumberRange(min=1)])
+    city = StringField('City', validators=[AnyOf(city_list)])
+    rooms = IntegerField('No. of rooms', validators=[NumberRange(min=1)])
+    min_price = IntegerField('Minimum price', validators=[NumberRange(min=0)])
+    max_price = IntegerField('Maximum price', validators=[NumberRange(min=1)])
     interiorType = SelectField('Interior/Furniture', choices=['Furnished', 'Unfurnished'])
-    size = StringField('Minimum size', validators=[DataRequired()])
+    size = IntegerField('Minimum size', validators=[NumberRange(min=0)])
     submit = SubmitField('Search for apartments')
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from forms import FilterForm
 
 app = Flask(__name__)
@@ -7,8 +7,17 @@ app.config['SECRET_KEY'] = '7215535b018cb068bf7e26cb750baa05'
 
 print(1)
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
 def home():
     form = FilterForm()
+    if form.validate_on_submit(): 
+        print(1)
+        return redirect(url_for('list'))
+    else:
+        print(2)
     return render_template('home.html', title='Home', form = form)
+
+@app.route("/list")
+def list():
+    return render_template('apartments_listing.html', title='List')
