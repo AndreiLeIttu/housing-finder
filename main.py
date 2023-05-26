21ce3a0584d5644c99728fc1030fd8ec5150aadd
Andrei Ionita <andrei.ionita2003@gmail.com>
2023-05-27 00:07:30 +0200

    Extracted data from the form in the form of a dictionary in order to search for the apartments using the filters provided by the user.

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 from flask import Flask, render_template, url_for, redirect
 from forms import FilterForm
+from apartments_getter import get_from_pararius
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '7215535b018cb068bf7e26cb750baa05'
 
-print(1)
+filters = {
+    "city" : "",
+    "minPrice" : "",
+    "maxPrice" : "",
+    "rooms" : "",
+    "interior" : "",
+    "size" : ""
+}   
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET', 'POST'])
 def home():
     form = FilterForm()
     if form.validate_on_submit(): 
-        print(1)
+        global filters
+        filters["city"]=form.city.data
+        filters["maxPrice"]=form.max_price.data
+        filters["minPrice"]=form.min_price.data
+        filters["rooms"]=form.rooms.data
+        filters["interior"]=form.interiorType.data
+        filters["size"]=form.size.data
         return redirect(url_for('list'))
     else:
         print(2)
@@ -20,4 +34,6 @@ def home():
 
 @app.route("/list")
 def list():
+    links = get_from_pararius(filters)
+    print(links)
     return render_template('apartments_listing.html', title='List')
